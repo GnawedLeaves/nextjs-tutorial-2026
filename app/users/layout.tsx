f4a6ff406c9a98d1d4f5197d0e617f0259882a34
Marcel <Marcelyap31@gmail.com>
2026-05-31 16:35:47 +0800

    metadata

--- app/users/layout.tsx
+++ app/users/layout.tsx
@@ -1,3 +1,15 @@
+import { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "My cool app",
+  description: "This is qwe qeqe qwe q eqw e metadata",
+  keywords: "about, company, company, mission,",
+  twitter: {
+    card: "summary_large_image",
+    title: "users | my cool app",
+  },
+};
+
 const UserLayout = ({ children }: { children: React.ReactNode }) => {
   return (
     <div>
